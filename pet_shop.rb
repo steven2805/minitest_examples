ab4646012f511c8061b9974f2ad6c9b6b8c4f4ec
steven2805 <stevenmac90@googlemail.com>
2017-05-26 13:34:29 +0100

    second commit

--- pet_shop.rb
+++ pet_shop.rb
@@ -6,10 +6,46 @@ def total_cash(total_cash)
   return total_cash[:admin][:total_cash]
 end 
 
-def add_or_remove_cash_add(pet_show, value)
-  sum = pet_shop[:admin][:total_cash] + value 
-  return sum 
+# def add_or_remove_cash_add(pet_shop, value)
+#   return pet_shop[:admin][:total_cash] + value 
+# end 
 
+def pets_sold(pets_sold)
+  return pets_sold[:admin][:pets_sold]
 end 
 
-return sum 
+def increase_pets_sold(pets_sold,sold)
+  return pets_sold[:admin][:pets_sold] = sold 
+end 
+
+def stock_count(pets_hash)
+  return pets_hash[:pets].count
+end
+
+def all_all_pets_by_breed_found(pets_hash,name)
+  result = 0
+  for pets_hash[:pets_shop][:pets][:pet_type].eql? name 
+    result += 1
+  end 
+return result
+end 
+
+
+
+    #   total = 0
+    #   for person in people
+    #     total += person[:monies]
+    #   end
+    #   return total
+    # end
+
+
+
+ 
+
+
+
+
+
+
+
