6ed52f6e69c5b0152c72fd72d10b3072fa5e046d
steven2805 <stevenmac90@googlemail.com>
2017-05-26 11:40:11 +0100

    first commit

---
+++ pet_shop.rb
@@ -0,0 +1,15 @@
+def pet_shop_name(pet_shop)
+  return pet_shop[:name]
+end 
+
+def total_cash(total_cash)
+  return total_cash[:admin][:total_cash]
+end 
+
+def add_or_remove_cash_add(pet_show, value)
+  sum = pet_shop[:admin][:total_cash] + value 
+  return sum 
+
+end 
+
+return sum 
